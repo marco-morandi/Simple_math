d2c727fa0cad93aeb807e02fca2c284da60463d2
Marco Morandi <marcomorandi3005@gmail.com>
2025-02-27 13:26:34 +0100

    first version of simple math -  no test

---
+++ simple_math.c
@@ -0,0 +1,10 @@
+/** \file simple_math:c
+    \brief a toy exampke of math library
+	\author Arturo Pardini
+	
+	
+*/
+#include "simple math .h"
+float mean (int a, int b){
+	return (a+b)/2;
+}
---
+++ simple_math.h
@@ -0,0 +1,13 @@
+/** \file simple_math:h
+    \brief a toy exampke of math library
+	\author Arturo Pardini
+	
+	
+*/
+
+#ifndef _SIMPLEMATH_   //se non è mai stato definito    così non ho problemi di redefinizione di funzioni
+#define _SIMPLEMATH_   //allora lo definisco
+
+float mean (int a, int b);
+
+#endif
